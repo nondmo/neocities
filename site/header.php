e04b0a5946703b1aa0962b7d65a0713f9689b2c1
nondmo <garm771@gmail.com>
2025-01-21 21:50:10 -0500

    create subsite for anomaly wip, update some links

---
+++ site/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>non dmo zone</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description" content="welcome to my twisted bra strap" />
+<script src="scripts.js"></script>
+<link rel="stylesheet" href="main.css">
+<link rel="icon" href="./assets/fav.ico">
+</head>
+
+<body>
+
+    
